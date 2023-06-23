050ea9eca0c7534c686d02253ea5e0da8b97b890
znilg <hglinz_bmw@hotmail.com>
2023-06-23 15:52:04 -0600

    V1.0
    V1.0

---
+++ ML/inputMessage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ML
+{
+    public class inputMessage
+    {
+        public string cfdi64 { get; set; }
+        public string cfdiState { get; set; }
+        public bool isFree { get; set; }
+        public string logo64 { get; set; }
+        public string informacionAdicional { get; set; }
+        public string infoNofiscal { get; set; }
+    }
+}
